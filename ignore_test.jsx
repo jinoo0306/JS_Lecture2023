022a82a4b8a5c679763104475b7ebd75a5b8edf4
j._.jinoo <jinoo0306@naver.com>
2023-12-31 02:29:26 +0900

    commit

--- ignore_test.jsx
+++ ignore_test.jsx
@@ -1,1 +1,12 @@
-import React
+function TestInputWithFocusbutton(props) {
+    const inputElem = useRef(null);
+
+    const onButtonClick = () => { inputElem.current.focus(); };
+
+    return (
+        <>
+            <input ref={inputElem} type="text" />
+            <button onClick={onButtonClick}>Focus the input</button>
+        </>
+    );
+}
